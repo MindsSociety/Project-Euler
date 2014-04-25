26bf846e842beb6fc6ebfcb48401928b41252065
Jack Higgins <jackhiggins07@gmail.com>
2014-04-25 11:03:51 +0100

    Create Problem56-Jack.hs

---
+++ Problem56/Problem56-Jack.hs
@@ -0,0 +1,6 @@
+digitsSum :: Integral a => a -> a
+digitsSum 0 = 0
+digitsSum x = r + digitsSum q
+    where (q,r) = quotRem x 10
+
+main = print . maximumBy (comparing length) . [a^b | a <- [1..99], b <- [1..99]]
