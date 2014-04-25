f0f081c977a41af0a0dbe8be68c8d70406f26718
Jack Higgins <jackhiggins07@gmail.com>
2014-04-25 20:16:27 +0100

    Create Problem2-Jack.hs

---
+++ Problem2/Problem2-Jack.hs
@@ -0,0 +1,8 @@
+{- 
+	By considering the terms in the Fibonacci sequence whose values do not
+	exceed four million, find the sum of the even-valued terms.
+-}
+
+fib :: [Integer] fib = 0 : 1 : zipWith (+) fib (tail fib)
+
+main = do     print . sum . filter even $ takeWhile (<= 4000000) fib
