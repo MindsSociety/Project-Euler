b294034c902b48fd02678c29d05116231655ec0f
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-07-13 10:51:07 +0100

    Update and rename PE003_Largest_prime_factor.java to sol.java

--- Problem003/sol.java
+++ Problem003/sol.java
@@ -5,11 +5,9 @@
  * The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime
  * factor of the number 600851475143 ?
  */
-public class PE003_Largest_prime_factor {
+public class sol {
 	public static void main(String[] args) {
-		long start = System.nanoTime();
-
-		long input = 600851475143L;
+		long input = 600851475143L, result = 0L;
 		int limit = (int) Math.sqrt(input) + 1;
 		boolean[] isPrime = new boolean[limit + 1];
 
@@ -23,18 +21,12 @@ public class PE003_Largest_prime_factor {
 			}
 		}
 
-		long result = 0L;
-
 		for (int i = 2; i < limit; ++i) {
 			if (!isPrime[i] && (input % i) == 0) {
 				result = Math.max(i, result);
 			}
 		}
 
-		long end = System.nanoTime();
-		long runtime = end - start;
 		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
 	}
 }
