49bceae8d9914b8d2a7745f99a9a1d3205eb5981
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-07-16 08:02:39 +0100

    Update and rename PE004_Largest_palindrome_product.java to sol.java

--- Problem004/sol.java
+++ Problem004/sol.java
@@ -6,17 +6,16 @@
  * from the product of two 2-digit numbers is 9009 = 91 × 99. Find the largest
  * palindrome made from the product of two 3-digit numbers.
  */
-
-public class PE004_Largest_palindrome_product {
+public class sol {
 	public static void main(String args[]) {
-		long start = System.nanoTime();
-
 		int result = 0, product = 0;
 		String palindrome = "";
+		
 		for (int i = 999; i > 99; i--) {
 			for (int j = 999; j > 99; j--) {
 				product = i * j;
 				palindrome = String.valueOf(product);
+				
 				if (new StringBuffer(palindrome).reverse().toString()
 						.equals(palindrome)
 						&& product > result) {
@@ -25,10 +24,6 @@ public class PE004_Largest_palindrome_product {
 			}
 		}
 
-		long end = System.nanoTime();
-		long runtime = end - start;
 		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
 	}
 }
