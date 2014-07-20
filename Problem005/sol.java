415916973e6e08bd58415ccc15ea08aa3834b07f
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-07-20 08:15:47 +0100

    Update sol.java

--- Problem005/sol.java
+++ Problem005/sol.java
@@ -14,13 +14,13 @@ public class sol {
 		while (!found) {
 			result++;
 
-			if (result % 11 == 0 && result % 12 == 0 && result % 13 == 0
-					&& result % 14 == 0 && result % 15 == 0 && result % 16 == 0
-					&& result % 17 == 0 && result % 18 == 0 && result % 19 == 0
-					&& result % 20 == 0) {
-				System.out.println(result);
+			if (result % 11 == 0 && result % 13 == 0 && result % 14 == 0
+					&& result % 16 == 0 && result % 17 == 0 && result % 18 == 0
+					&& result % 19 == 0 && result % 20 == 0) {
 				found = true;
 			}
 		}
+
+		System.out.println(result);
 	}
 }
