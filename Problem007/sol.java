8a576efc28db2ab3d583435c053c2d5c7d8c6b5b
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-07-22 08:04:27 +0100

    Update and rename PE007_10001st_prime.java to sol.java

--- Problem007/sol.java
+++ Problem007/sol.java
@@ -5,21 +5,22 @@
  * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
  * that the 6th prime is 13. What is the 10,001st prime number?
  */
-public class PE007_10001st_prime {
+public class sol {
 	public static void main(String[] args) {
-		long start = System.nanoTime();
+		int j, counter = 0, result = 0;
 
-		int y, c = 0, result = 0;
 		for (int i = 2; i < 110000; i++) {
 			if (i % 2 != 0 || i == 2) {
-				for (y = 2; y <= i / 2; y++) {
-					if (i % y == 0) {
+				for (j = 2; j <= i / 2; j++) {
+					if (i % j == 0) {
 						break;
 					}
 				}
-				if (y > i / 2) {
-					c++;
-					if (c == 10001) {
+
+				if (j > i / 2) {
+					counter++;
+
+					if (counter == 10001) {
 						result = i;
 						break;
 					}
@@ -27,10 +28,6 @@ public class PE007_10001st_prime {
 			}
 		}
 
-		long end = System.nanoTime();
-		long runtime = end - start;
 		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
 	}
 }
