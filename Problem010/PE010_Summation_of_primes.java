19c9a78015dbc890878900470464e7cd5cd5f098
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-07-26 09:51:15 +0100

    Update PE010_Summation_of_primes.java

--- Problem010/PE010_Summation_of_primes.java
+++ Problem010/PE010_Summation_of_primes.java
@@ -9,18 +9,13 @@ import java.math.BigInteger;
  */
 public class PE010_Summation_of_primes {
 	public static void main(String[] args) {
-		long start = System.nanoTime();
-
 		BigInteger result = BigInteger.ZERO;
+		
 		for (BigInteger num = BigInteger.valueOf(2); num.compareTo(BigInteger
 				.valueOf(2000000)) < 0; num = num.nextProbablePrime()) {
 			result = result.add(num);
 		}
 
-		long end = System.nanoTime();
-		long runtime = end - start;
 		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
 	}
 }
