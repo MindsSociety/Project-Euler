cfc5b1cd7af8d447487043b56c0046104af237ac
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-07-30 10:49:55 +0100

    Update and rename PE012_Highly_divisible_triangular_number.java to sol.java

--- Problem012/sol.java
+++ Problem012/sol.java
@@ -14,35 +14,36 @@ public class PE012_Highly_divisible_triangular_number {
 	private static int result;
 	
 	public static void main(String[] args) {
-		long start = System.nanoTime();
 		int counter = 0;
+		
 		for (int i = 1; counter <= 500; i++) {
 			if (i % 2 == 0) {
 				counter = count(i / 2) * count(i + 1);
 			} else {
 				counter = count(i) * count((i + 1) / 2);
 			}
+			
 			if (counter > 500) {
 				result = i;
 			}
 		}
-		long end = System.nanoTime();
-		long runtime = end - start;
+		
 		System.out.println(result * (result + 1) / 2);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
 	}
 
 	private static int count(int n) {
 		int result = 0;
+		
 		for (int i = 1; i * i <= n; i++) {
 			if (n % i == 0) {
 				result += 2;
+				
 				if (n / i == i) {
 					result--;
 				}
 			}
 		}
+		
 		return result;
 	}
 }
