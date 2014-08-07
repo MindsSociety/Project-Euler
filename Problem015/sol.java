4f1963deb06f2b4ee3cd8c1040c7cc7fd98a9ff7
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-08-07 10:29:28 +0100

    Update and rename PE015_Lattice_paths.java to sol.java

--- Problem015/sol.java
+++ Problem015/sol.java
@@ -6,15 +6,13 @@
  * the right and down, there are exactly 6 routes to the bottom right corner.
  * How many such routes are there through a 20×20 grid?
  */
-public class PE015_Lattice_paths {
+public class sol {
 	public static void main(String[] args) {
-		long start = System.nanoTime();
-
-		long result;
 		long [] right = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 				1, 1, 1 };
 		long [] current = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 				0, 0, 0, 0 };
+				
 		for (int x = 0; x < 20; x++) {
 			for (int y = 0; y < 21; y++) {
 				if (y == 0) {
@@ -23,14 +21,10 @@ public class PE015_Lattice_paths {
 					current[y] = current[y - 1] + right[y];
 				}
 			}
+			
 			right = current;
 		}
-		result = current[20];
-
-		long end = System.nanoTime();
-		long runtime = end - start;
-		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
+		
+		System.out.println(current[20]);
 	}
 }
