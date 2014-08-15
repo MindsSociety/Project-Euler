afe010e38257daa013acc85831c03a7b3754f9dc
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-08-15 07:43:52 +0100

    Update and rename PE020_Factorial_digit_sum.java to sol.java

--- Problem020/sol.java
+++ Problem020/sol.java
@@ -8,10 +8,8 @@ import java.math.BigInteger;
  * 2 * 1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8
  * + 8 + 0 + 0 = 27. Find the sum of the digits in the number 100!
  */
-public class PE020_Factorial_digit_sum {
+public class sol {
 	public static void main(String[] args) {
-		long start = System.nanoTime();
-
 		BigInteger bi1 = BigInteger.valueOf(100);
 		int result = 0;
 
@@ -19,16 +17,13 @@ public class PE020_Factorial_digit_sum {
 			BigInteger bi2 = BigInteger.valueOf(i);
 			bi1 = bi1.multiply(bi2);
 		}
+		
 		String big = bi1.toString();
 
 		for (int i = 0; i < big.length(); i++) {
 			result += Integer.parseInt(big.substring(i, i + 1));
 		}
 
-		long end = System.nanoTime();
-		long runtime = end - start;
 		System.out.println(result);
-		System.out.println("Runtime: " + runtime / 1000000 + "ms (" + runtime
-				+ "ns)");
 	}
 }
